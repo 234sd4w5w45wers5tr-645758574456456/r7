a8478637e553669c98f3d22ac4bbf3d4bcd73a7e
Games Unblocked <69767501+unblocked-games-premium@users.noreply.github.com>
2023-04-11 00:53:07 +0300

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -1,12 +1,12 @@
 // This file overwrites the stock UV config.js
 
 self.__uv$config = {
-  prefix: "/uv/service/",
+  prefix: "/service/",
   bare: "/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: "/uv/uv.handler.js",
-  bundle: "/uv/uv.bundle.js",
-  config: "/uv/uv.config.js",
-  sw: "/uv/uv.sw.js",
+  handler: "uv.handler.js",
+  bundle: "uv.bundle.js",
+  config: "uv.config.js",
+  sw: "uv.sw.js",
 };
